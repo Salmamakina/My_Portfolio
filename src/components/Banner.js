3a517aedc9a3bf18f85054f78848a3f531cc5453
salma <salma.makinaa2557@gmail.com>
2024-06-23 02:17:47 +0100

    skills nd projects commit

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,5 +1,5 @@
-import { useState, useEffectffect } from "react";
-import { Container, Row } from "react-bootstrap";
+import { useState, useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
 import { ArrowRightCircle } from "react-bootstrap-icons";
 import headerImg from "../assets/img/header-img.svg";
 
@@ -20,7 +20,23 @@ export const Banner = () =>{
     }, [text])
 
     const tick =() =>{
-        
+        let i = loopNum % toRotate.length;
+        let fullText = toRotate[i];
+        let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+        setText(updatedText);
+
+        if (isDeleting) {
+            setDelta(prevDelta => prevDelta /2)
+        }
+        if (!isDeleting && updatedText === fullText){
+            setIsDeleting(true);
+            setDelta(period);
+        }
+        else if(isDeleting && updatedText === ''){
+            setIsDeleting(false);
+            setLoopNum(loopNum + 1);
+            setDelta(500);
+        } 
     }
     return (
         <section className="banner" id="home">
@@ -28,12 +44,12 @@ export const Banner = () =>{
                 <Row className="align-items-center">
                     <Col xs={12} md={6} xl={7}>
                         <span className="tagline">Welcome to my Portfolio</span>
-                        <h1>{`Hi I'm webdecoded`}<span>Web developer</span></h1>
+                        <h1>{`Hi I'm Salma Makina `}<span>{text}</span></h1>
                         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
                         <button onClick={() => console.log('connect')}>Let's connect <ArrowRightCircle  size={25}/> </button>
                     </Col>
                     <Col xs={12} md={6} xl={5}>
-                        <img src={""} alt="Headder Img" />
+                        <img src={headerImg} alt="Headder Img" />
                     </Col>
                 </Row>
             </Container>
